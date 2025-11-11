876149ebdd78692f7777d4fe9c95de668fc7f53e
Muhammad-Saad-12853 <saadkhan.developer12@gmail.com>
2025-11-11 15:20:03 +0500

    update

--- src/app/components/booking/booking.ts
+++ src/app/components/booking/booking.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { BookingService } from '../../services/booking-service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-booking',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './booking.html',
   styleUrl: './booking.css',
 })
-export class Booking {
+export class Booking implements OnInit {
+  bookingsrv = inject(BookingService);
+  carList: any[] = [];
+  bookingList: any[] = []
+  bookingForm: FormGroup = new FormGroup({
+    customerName: new FormControl(""),
+    customerCity: new FormControl(""),
+    mobileNo: new FormControl(""),
+    emailId: new FormControl(""),
+    bookingId: new FormControl(0),
+    carId: new FormControl(""),
+    bookingDate: new FormControl(""),
+    discount: new FormControl(""),
+    totalBillAmount: new FormControl("")
+  })
+  ngOnInit(): void {
+    this.getCarList();
+    this.getBookings();
+  }
+  getCarList() {
+    this.bookingsrv.getAllCars().subscribe((res: any) => {
+      this.carList = res.data
+    })
+  }
+  getBookings() {
+    this.bookingsrv.getAllBooking().subscribe((res: any) => {
+      this.bookingList = res.data
+    })
+  }
 
+  onSave() {
+    if (this.bookingForm.valid) {
+      const formValue = this.bookingForm.value;
+      console.log('Form Value:', formValue);
+      this.bookingsrv.saveBooking(formValue).subscribe({
+        next: (res: any) => {
+          console.log('API Response:', res);
+          if (res.result) {
+            alert("Booking Done");
+            this.bookingForm.reset();
+            this.getBookings(); // Refresh the list
+          } else {
+            alert(res.message);
+          }
+        },
+        error: (err: any) => {
+          console.error('API Error:', err);
+          alert('Error saving booking: ' + err.message);
+        }
+      });
+    } else {
+      alert('Please fill all required fields correctly.');
+    }
+  }
 }
---
+++ src/app/services/booking-service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookingService {
+  constructor(private http:HttpClient){
+
+  }
+  getAllBooking(){
+    return this.http.get("https://freeapi.miniprojectideas.com/api/CarRentalApp/geAllBookings")
+  }
+  getAllCars(){
+        return this.http.get("https://freeapi.miniprojectideas.com/api/CarRentalApp/GetCars")
+  }
+
+  saveBooking(obj:any) {
+      console.log('Sending booking data:', obj);
+      return this.http.post("https://freeapi.miniprojectideas.com/api/CarRentalApp/CreateNewBooking",obj)
+    }
+      
+}
